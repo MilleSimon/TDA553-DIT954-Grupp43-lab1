da27410b6fc89ebbbdb7ea63c95ff46fad14b0b1
Hannah dagemark <dagemark@student.chalmers.se>
2026-01-29 09:17:33 +0000

    Test updates

--- CarCollection.java
+++ CarCollection.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class CarCollection {
+    //static final ArrayList<Car> cars = {}
     public static ArrayList<Car> getAllCars() {
         return new ArrayList<>();
     }
--- Volvo240Test.java
+++ Volvo240Test.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Volvo240Test {
+class Volvo240Test extends CarTest{
 
     @Test
     void TestVolvo240() {
