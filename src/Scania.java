139825b05c752b9d13066563f7607658746ece75
Hannah Dagemark <hannah.dagemark@gmail.com>
2026-02-03 02:33:10 +0100

    Initialize Scania Class

---
+++ src/Scania.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Scania extends Car{
+    public Scania(){
+        super(2, 50, Color.gray, "Scania");
+    }
+
+    public int getRampAngle() {
+        // TODO
+        return -1;
+    }
+    public void liftRamp(int angle) {
+        // TODO
+    }
+    public void lowerRamp(int angle) {
+        // TODO
+    }
+}
