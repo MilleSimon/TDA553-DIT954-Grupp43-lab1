f61ea213d74b1ead859eb984b2d01eaa29fbeda9
Hannah Dagemark <hannah.dagemark@gmail.com>
2026-02-03 17:34:44 +0100

    Initial Pseudocode, awaiting Ramp class

--- src/Scania.java
+++ src/Scania.java
@@ -1,18 +1,28 @@
 import java.awt.*;
 
-public class Scania extends Car{
+public class Scania extends Car {
+
+
     public Scania(){
         super(2, 50, Color.gray, "Scania");
+        ramp = new DegreeRamp();
     }
 
     public int getRampAngle() {
-        // TODO
-        return -1;
+        return ramp.getAngle();
     }
     public void liftRamp(int angle) {
-        // TODO
+        if (ramp.getAngle() + angle < 70) {
+            ramp.lift(angle);
+        } else {
+            ramp.lift(70 - ramp.getAngle());
+        }
     }
     public void lowerRamp(int angle) {
-        // TODO
+        if (ramp.getAngle() - angle > 0) {
+            ramp.lower(angle);
+        } else {
+            ramp.lower(ramp.getAngle());
+        }
     }
 }
