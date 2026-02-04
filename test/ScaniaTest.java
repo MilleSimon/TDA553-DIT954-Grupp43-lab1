3140626a4de71e1b04ba9f28e835fe494f53da3d
Hannah Dagemark <hannah.dagemark@gmail.com>
2026-02-04 17:27:35 +0100

    Update scania test to align with new names

--- test/ScaniaTest.java
+++ test/ScaniaTest.java
@@ -16,9 +16,9 @@ public class ScaniaTest extends CarTest {
         // Make sure the instance properly initiated
         assertEquals("Scania", instanceScania.getModelName());
 
-        assertEquals(0, instanceScania.getRampAngle()); // Starting angle should be 0, Scania should have getRampAngle method
-        instanceScania.liftRamp(10); // Attempt to tip whilst in motion, Scania should have liftRamp method
-        assertFalse(instanceScania.getRampAngle() > 0); // Make sure that the ramp could not be lifted whilst in motion
+        assertEquals(0, instanceScania.getFlatBedAngle()); // Starting angle should be 0, Scania should have getFlatBedAngle method
+        instanceScania.raiseFlatBed(10); // Attempt to tip whilst in motion, Scania should have raiseFlatBed method
+        assertFalse(instanceScania.getFlatBedAngle() > 0); // Make sure that the ramp could not be lifted whilst in motion
     }
 
     @Test
@@ -26,12 +26,12 @@ public class ScaniaTest extends CarTest {
         Scania instanceScania = new Scania();
         instanceScania.stopEngine();
 
-        int testAngle = instanceScania.getRampAngle();
-        instanceScania.liftRamp(10);
-        assertFalse(instanceScania.getRampAngle() <= testAngle);
-        testAngle = instanceScania.getRampAngle();
-        instanceScania.lowerRamp(5);
-        assertFalse(instanceScania.getRampAngle() >= testAngle);
+        int testAngle = instanceScania.getFlatBedAngle();
+        instanceScania.raiseFlatBed(10);
+        assertFalse(instanceScania.getFlatBedAngle() <= testAngle);
+        testAngle = instanceScania.getFlatBedAngle();
+        instanceScania.lowerFlatBed(5);
+        assertFalse(instanceScania.getFlatBedAngle() >= testAngle);
     }
 
     @Test
@@ -39,23 +39,23 @@ public class ScaniaTest extends CarTest {
         Scania instanceScania = new Scania();
         instanceScania.stopEngine();
 
-        int testAngle = instanceScania.getRampAngle();
-        int previousTestAngle = instanceScania.getRampAngle();
+        int testAngle = instanceScania.getFlatBedAngle();
+        int previousTestAngle = instanceScania.getFlatBedAngle();
 
-        while (testAngle != previousTestAngle && previousTestAngle < 70) { // This loop will only exit once the ramp has increased to 70, and then remained at 70 after a subsequent liftRamp call. Test failure exits while-loop for edge cases
+        while (testAngle != previousTestAngle && previousTestAngle < 70) { // This loop will only exit once the ramp has increased to 70, and then remained at 70 after a subsequent raiseFlatBed call. Test failure exits while-loop for edge cases
             assertFalse(testAngle > 70); // Angle should not be able to go above 70
-            assertFalse(testAngle < previousTestAngle); // Angle should not decrease from "liftRamp" method
-            previousTestAngle = instanceScania.getRampAngle();
-            instanceScania.liftRamp(5);
-            testAngle = instanceScania.getRampAngle();
+            assertFalse(testAngle < previousTestAngle); // Angle should not decrease from "raiseFlatBed" method
+            previousTestAngle = instanceScania.getFlatBedAngle();
+            instanceScania.raiseFlatBed(5);
+            testAngle = instanceScania.getFlatBedAngle();
         }
 
-        while (testAngle != previousTestAngle && previousTestAngle != 0) { // This loop will only exit once the ramp has decreased to 0, and then remained at 0 after a subsequent lowerRamp call. - II -
+        while (testAngle != previousTestAngle && previousTestAngle != 0) { // This loop will only exit once the ramp has decreased to 0, and then remained at 0 after a subsequent lowerFlatBed call. - II -
             assertFalse(testAngle < 0); // Angle should not be able to go below 0
-            assertFalse(testAngle > previousTestAngle); // Angle should not decrease from "liftRamp" method
-            previousTestAngle = instanceScania.getRampAngle();
-            instanceScania.lowerRamp(5);
-            testAngle = instanceScania.getRampAngle();
+            assertFalse(testAngle > previousTestAngle); // Angle should not decrease from "raiseFlatBed" method
+            previousTestAngle = instanceScania.getFlatBedAngle();
+            instanceScania.lowerFlatBed(5);
+            testAngle = instanceScania.getFlatBedAngle();
         }
 
     }
