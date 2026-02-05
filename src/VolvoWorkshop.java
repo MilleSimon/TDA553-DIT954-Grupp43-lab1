bbae626824a7705b4a4ac1475de36fc7776a677d
MilleSimon <isak.logenius@gmail.com>
2026-02-05 14:17:17 +0100

    VolvoWorkshop here

---
+++ src/VolvoWorkshop.java
@@ -0,0 +1,11 @@
+public class VolvoWorkshop extends Workshop {
+
+    public VolvoWorkshop(Position position, Rotation rotation) {
+        super(position, rotation);
+    }
+
+    @Override
+    public boolean load(Car item) {
+
+    }
+}
